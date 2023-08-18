8a8bf6383defc2638ea167fcb649bb8aa4b14176
rodolphoCwb <rodolphoribeiro@hotmail.com>
2023-08-18 10:33:41 -0300

    Classes push

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        listaFuncionarios();
+        //Basicamente um teste pode ignorar
+        listaFuncionarios();//falta coisa aqui
     }
 
 
